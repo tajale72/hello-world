121b7bc954beee0091434ce23810793b55ee0691
Tajale72 <Tajale72@gmail.com>
2022-03-10 16:26:42 -0600

    my first app-deployment

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hellow world")
+	port := os.Getenv("PORT")
+	router := gin.Default()
+	router.GET("/", Hello)
+	router.Run(port)
+}
+
+func Hello(c *gin.Context) {
+	c.JSON(http.StatusAccepted, "hello world")
 }
